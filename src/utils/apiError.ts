4d6956dead87d6fad68ba6e0bc524cd0417b2660
Tanish Singla <131950694+tanishsingla51@users.noreply.github.com>
2025-09-03 15:44:57 +0530

    Add src/utils/apiError.ts

---
+++ src/utils/apiError.ts
@@ -0,0 +1,33 @@
+export class ApiError extends Error {
+  statusCode: number;
+
+  constructor(statusCode: number, message: string) {
+    super(message);
+    this.statusCode = statusCode;
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
+}
+
+export class BadRequestError extends ApiError {
+  constructor(message = 'Bad Request') {
+    super(400, message);
+  }
+}
+
+export class UnauthorizedError extends ApiError {
+  constructor(message = 'Unauthorized') {
+    super(401, message);
+  }
+}
+
+export class ForbiddenError extends ApiError {
+  constructor(message = 'Forbidden') {
+    super(403, message);
+  }
+}
+
+export class NotFoundError extends ApiError {
+  constructor(message = 'Not Found') {
+    super(404, message);
+  }
+}
